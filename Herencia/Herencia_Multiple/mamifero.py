8b073467701c682ae192a100588c072eecd6f591
Miguel21S <leaismiguelss@gmail.com>
2025-03-16 07:53:57 +0100

    feat: Primeiro exemplo de Herencia múltiple

---
+++ Herencia/Herencia_Multiple/mamifero.py
@@ -0,0 +1,18 @@
+
+class Mamifero:
+    def amamentar(self):
+        print("Esté animal é um mamífero.")
+
+class Voador:
+    def voar(self):
+        print("Esté animal pode voar")
+
+class Murciego(Mamifero, Voador):
+    def fazer_ruido(self):
+        print("chirp, chirp")
+        
+
+bat = Murciego()
+bat.amamentar()
+
+bat.fazer_ruido()
